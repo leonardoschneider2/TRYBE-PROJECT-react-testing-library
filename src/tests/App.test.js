2f24213ed33d604e451bde48ffec23effe658b08
leonardo-schneider2 <leocoelho256@gmail.com>
2022-07-18 22:01:31 -0300

    Começando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Vamos testar o componente App: ', () => {
+  test('Verificando o Topo da aplicação: ', () => {
+    render(<App />);
+  });
+});
