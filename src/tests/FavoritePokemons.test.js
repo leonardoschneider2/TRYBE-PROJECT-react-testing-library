2dd59b47bfee11362ee9235823b73c2af6bc2424
leonardo-schneider2 <leocoelho256@gmail.com>
2022-07-20 22:07:30 -0300

    testes 03 e 04

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente Favorite Pokemons', () => {
+  test('Testando quando Favorite Pokemons estiver sem pokemon favoritado: ', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoPokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoPokemon).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente Favorite Pokemons', () => {
+  test('Testando quando Favorite Pokemons estiver sem pokemon favoritado: ', () => {
+    renderWithRouter(<NotFound />);
+
+    const pageRequest = screen.getByText(/Page Requested Not Found/i);
+    expect(pageRequest).toBeInTheDocument();
+
+    const image = screen.getByAltText(/Pikachu Crying/i);
+    console.log(image);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
